2823b3a2b71d473712caef0bfb3b767e6070d7d5
dwayne9078 <pabloroo87@gmail.com>
2024-04-08 01:23:25 -0600

    changes to config

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,11 @@ import fs from "fs";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
-const privateKey = fs.readFileSync("config/key.pem");
-const certificate = fs.readFileSync("config/cert.pem", "utf-8");
+const privateKey = fs.readFileSync("src/config/key.pem");
+const certificate = fs.readFileSync("src/config/cert.pem", "utf-8");
 const port = process.env.PORT || 3000;
 
 const credentials = { key: privateKey, cert: certificate };
-
-connectDB();
 
 /* DESARROLLO */
 // app.listen(3000);
@@ -20,3 +18,4 @@ connectDB();
 https
   .createServer(credentials, app)
   .listen(port, "0.0.0.0", () => console.log("Up"));
+connectDB();
