348889cc500dff8f00f8cdd57a6740a2e4af0f63
dwayne9078 <pabloroo87@gmail.com>
2024-04-09 10:53:02 -0600

    mas produccion

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://login-b2f58.web.app",
     // preflightContinue: true,
     credentials: true,
   })
