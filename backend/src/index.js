4d294137f4b7374fc8b72acf5a8e7e36554b62b5
dwayne9078 <pabloroo87@gmail.com>
2024-04-08 23:19:28 -0600

    mas cambios produccion

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,23 @@
-import https from "https";
-import fs from "fs";
+// import https from "https";
+// import fs from "fs";
 
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
-const privateKey = fs.readFileSync("src/config/key.pem");
-const certificate = fs.readFileSync("src/config/cert.pem", "utf-8");
+// const privateKey = fs.readFileSync("src/config/key.pem");
+// const certificate = fs.readFileSync("src/config/cert.pem", "utf-8");
+// const credentials = { key: privateKey, cert: certificate };
+
 const port = process.env.PORT || 3000;
 
-const credentials = { key: privateKey, cert: certificate };
-
 /* DESARROLLO */
-// app.listen(3000);
-// console.log("Up");
+app.listen(port, () => {
+  console.log("Up");
+  connectDB();
+});
 
 /* PRODUCCION */
-https
-  .createServer(credentials, app)
-  .listen(port, "0.0.0.0", () => console.log("Up"));
-connectDB();
+// https
+//   .createServer(credentials, app)
+//   .listen(port, "0.0.0.0", () => console.log("Up"));
+// connectDB();
