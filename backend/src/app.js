28c1993426c34f48f9eba1da326c41611879a728
dwayne9078 <pabloroo87@gmail.com>
2024-04-17 08:19:10 -0600

    backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    // origin: "*",
     // origin: "http://localhost:5173",
     // preflightContinue: true,
     credentials: true,
