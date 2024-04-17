f4e8815cd2c42e1ae00f5ca47a568bff499934c5
dwayne9078 <pabloroo87@gmail.com>
2024-04-17 09:19:55 -0600

    backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const DB_NAME = "login";
 // const MONGO_URL = `mongodb://localhost:27017/${DB_NAME}`;
-const MONGO_URL = `mongodb+srv://dwayne:3qWdNCFyu2jnBFAi@cluster0.zjlx17j.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+const MONGO_URL = `mongodb+srv://liz:mwdTS2LoxjveCL8k@cluster0.qpjn9lk.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 export const connectDB = async () => {
   try {
